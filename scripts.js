f7e6c6f7b2511dd32cbd3d3220afa13a71653621
Maciej Wlazło <carenauur@gmail.com>
2022-04-27 14:43:54 +0200

    added elements

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
 class CookiePolicyService {
-    storageType = window.localStorage;
+    cookieStorage = {
+        getItem: (key) => {
+            let cookies = document.cookie
+                .split(';')
+                .map(cookie => cookie.split('='))
+                .reduce((acc, [key, value]) => ({ ...acc, [key.trim()]: value }), {});
+            return cookies[key];
+        },
+        setItem: (key, value) => {
+            document.cookie = `${key}=${value}`;
+        }
+    };
+
+    storageType = this.cookieStorage;
     consentPropertyName = 'jdc_consent';
 
-    constructor(policyUrl) {
+    constructor(policyUrl = '#') {
         this.policyUrl = policyUrl;
         this.self = this;
     }
@@ -34,13 +47,13 @@ class CookiePolicyService {
         console.log(
             this.storageType,
             this.shouldShowPopup(),
+            this.checkServiceCookie(),
         );
     }
 
-    f() {
+    checkServiceCookie() {
         if (this.shouldShowPopup()) {
-            if( this.createCookieAgreement() )
-            {
+            if (this.createCookieAgreement()) {
                 this.saveToStorage();
             }
         }
@@ -52,19 +65,37 @@ class CookiePolicyService {
     }
 
     saveToStorage() {
-        return this.storageType.getItem(this.consentPropertyName, true);
+        return this.storageType.setItem(this.consentPropertyName, true);
     }
 
     createCookieSerivcePopup() {
+        let popupWrapper = document.createElement('div'),
+            popupContainer = document.createElement('div'),
+            popupP = document.createElement('p'),
+            popupBtn = document.createElement('div');
+
+        popupWrapper.classList.add('cookieService-wrapper');
+        popupWrapper.style.cssText('z-index: 9999;width: 100 %;height: auto;position: absolute;bottom: 0;background - color: #fff;padding: 15px 0px;-webkit - box - shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;box - shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;display: -webkit - box;display: -ms - flexbox;display: flex;-webkit - box - orient: horizontal;-webkit - box - direction: normal;-ms - flex - direction: row;flex - direction: row;-webkit - transition: bottom ease - out 200ms;transition: bottom ease - out 200ms;'.trim());
+
+        popupContainer.classList.add('cookieService-container');
+        popupContainer.style.cssText('-webkit-box-flex: 0;-ms-flex: 0 0 60%;flex: 0 0 60%;margin: 0 auto;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: horizontal;-webkit-box-direction: normal;-ms-flex-direction: row;flex-direction: row;'.trim());
+
+        popupP.classList.add('consent');
+        popupP.style.cssText = 
+
+        popupBtn.addEventListener('click', this.accept());
 
     }
 
     createCookieAgreement() {
 
+        this.createCookieSerivcePopup();
+
+        return true;
     }
 
-    createCookieSerivce() {
-
+    accept(event) {
+        this.saveToStorage(this.storageType);
     }
 
 }
