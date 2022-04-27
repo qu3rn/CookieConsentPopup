6c90a32e882517e7380d6186dffc79bb66e96070
Maciej Wlazło <carenauur@gmail.com>
2022-04-27 15:41:31 +0200

    creating dom elements

--- scripts.js
+++ scripts.js
@@ -44,10 +44,12 @@ class CookiePolicyService {
 
     cookieServiceStart() {
         // walktrought function
+        
         console.log(
             this.storageType,
             this.shouldShowPopup(),
             this.checkServiceCookie(),
+            
         );
     }
 
@@ -68,30 +70,39 @@ class CookiePolicyService {
         return this.storageType.setItem(this.consentPropertyName, true);
     }
 
-    createCookieSerivcePopup() {
+    createCookieSerivcePopupDOM() {
+
         let popupWrapper = document.createElement('div'),
             popupContainer = document.createElement('div'),
             popupP = document.createElement('p'),
             popupBtn = document.createElement('div');
 
         popupWrapper.classList.add('cookieService-wrapper');
-        popupWrapper.style.cssText('z-index: 9999;width: 100 %;height: auto;position: absolute;bottom: 0;background - color: #fff;padding: 15px 0px;-webkit - box - shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;box - shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;display: -webkit - box;display: -ms - flexbox;display: flex;-webkit - box - orient: horizontal;-webkit - box - direction: normal;-ms - flex - direction: row;flex - direction: row;-webkit - transition: bottom ease - out 200ms;transition: bottom ease - out 200ms;'.trim());
-
         popupContainer.classList.add('cookieService-container');
-        popupContainer.style.cssText('-webkit-box-flex: 0;-ms-flex: 0 0 60%;flex: 0 0 60%;margin: 0 auto;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: horizontal;-webkit-box-direction: normal;-ms-flex-direction: row;flex-direction: row;'.trim());
-
         popupP.classList.add('consent');
-        popupP.style.cssText = 
+        popupBtn.classList.add('cookieService-btn');
 
-        popupBtn.addEventListener('click', this.accept());
+        popupP.innerHTML = `Plikami cookies stosowanymi na naszej stronie można zarządzać, w tym je usunąć za pośrednictwem ustawień przeglądarki internetowej. Internauci mogą dowolnie zarządzać stosowanymi plikami <a href="${this.policyUrl}">(polityka prywatności)</a>`;
+        popupBtn.innerHTML = `Ok`;   
+        
+        popupWrapper.appendChild(popupContainer);
+        popupContainer.appendChild(popupP);
+        popupContainer.appendChild(popupBtn);
+        document.body.appendChild(popupWrapper);
+
+        popupWrapper.style.cssText = 'z-index: 9999;width: 100 %;height: auto;position: absolute;bottom: 0;background - color: #fff;padding: 15px 0px;-webkit - box - shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;box - shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;display: -webkit - box;display: -ms - flexbox;display: flex;-webkit - box - orient: horizontal;-webkit - box - direction: normal;-ms - flex - direction: row;flex - direction: row;-webkit - transition: bottom ease - out 200ms;transition: bottom ease - out 200ms;'.trim();
+        popupContainer.style.cssText = '-webkit-box-flex: 0;-ms-flex: 0 0 60%;flex: 0 0 60%;margin: 0 auto;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: horizontal;-webkit-box-direction: normal;-ms-flex-direction: row;flex-direction: row;'.trim();
+        popupP.style.cssText = 'font-family: "Roboto", sans-serif;vertical-align: middle;text-align: justify;font-size: 12px;padding-right: 13px;'.trim();
+
+        popupBtn.addEventListener('click', () => { this.accept() });
 
     }
 
     createCookieAgreement() {
 
-        this.createCookieSerivcePopup();
+        this.createCookieSerivcePopupDOM();
 
-        return true;
+        return false;
     }
 
     accept(event) {
@@ -101,4 +112,5 @@ class CookiePolicyService {
 }
 
 service = new CookiePolicyService;
+service.createCookieSerivcePopupDOM();
 service.cookieServiceStart();
