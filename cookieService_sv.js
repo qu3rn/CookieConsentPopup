5fba8dcdb0bb8b5964ea4eb7171c939283c23486
Maciej Wlazło <carenauur@gmail.com>
2022-04-28 16:00:14 +0200

    finished script with init fnc

--- cookieService_sv.js
+++ cookieService_sv.js
@@ -79,7 +79,7 @@ class CookiePolicyService {
         animSpeed = animSpeed ? animSpeed : this.animBoxSpeed;
         const transition = `opacity ${animSpeed}ms ease`;
         const btnBgColorBase = '#0CF25D';
-        const btnBgColorHover = '#06732C'
+        const btnBgColorHover = '#06732C';
 
         //console.log(url, '\n', animSpeed);
 
@@ -88,9 +88,9 @@ class CookiePolicyService {
             popupP = document.createElement('p'),
             popupBtn = document.createElement('div'),
             link = document.createElement('link'),
-            stylesWrapper = `z-index: 9999;width: 100%;height: auto;position: absolute;bottom: 0;background-color: rgba(39, 59, 64,0.25);padding: 8px 0px;-webkit-box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: horizontal;-webkit-box-direction: normal;-ms-flex-direction: row;flex-direction: row;-webkit-transition: ${transition};transition: ${transition};`.trim(),
+            stylesWrapper = `z-index: 9999;width: 100%;height: auto;position: absolute;bottom: 0;background-color: rgba(39, 59, 64,0.45);padding: 8px 0px;-webkit-box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: horizontal;-webkit-box-direction: normal;-ms-flex-direction: row;flex-direction: row;-webkit-transition: ${transition};transition: ${transition};`.trim(),
             stylesContainer = '-webkit-box-flex: 0;-ms-flex: 0 0 60%;flex: 0 0 60%;margin: 0 auto;display: -webkit-box;display: -ms-flexbox;display: flex;-webkit-box-orient: horizontal;-webkit-box-direction: normal;-ms-flex-direction: row;flex-direction: row;'.trim(),
-            stylesP = 'font-family: "Roboto", sans-serif;vertical-align: middle;text-align: justify;font-size: 10px;padding-right: 13px;'.trim(),
+            stylesP = 'margin: auto !important;line-height: 12px;color: #f2f2f2;font-family: "Roboto", sans-serif;vertical-align: middle;text-align: justify;font-size: 12px;padding-right: 47px;'.trim(),
             stylesBtn = `-webkit-box-align: center; -ms-flex-align: center; align-items: center; background-color: ${btnBgColorBase}; border: 0; border-radius: 100px; -webkit-box-sizing: border-box; box-sizing: border-box; color: #ffffff; cursor: pointer; display: -webkit-inline-box; display: -ms-inline-flexbox; display: inline-flex; font-family: -apple-system, system-ui, system-ui, "Segoe UI", Roboto, "Helvetica Neue", "Fira Sans", Ubuntu, Oxygen, "Oxygen Sans", Cantarell, "Droid Sans", "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Lucida Grande", Helvetica, Arial, sans-serif; font-size: 16px; font-weight: 600; -webkit-box-pack: center; -ms-flex-pack: center; justify-content: center; line-height: 20px; max-width: 480px; min-height: 40px; min-width: 0px; overflow: hidden; padding: 0px; padding-left: 20px; padding-right: 20px; text-align: center; -ms-touch-action: manipulation; touch-action: manipulation; -webkit-transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, -webkit-box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s; transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, -webkit-box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s; transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s; transition: background-color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, color 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s, -webkit-box-shadow 0.167s cubic-bezier(0.4, 0, 0.2, 1) 0s; -moz-user-select: none; -ms-user-select: none; user-select: none; -webkit-user-select: none; vertical-align: middle;`.trim();
 
         console.log(stylesWrapper);
@@ -105,7 +105,7 @@ class CookiePolicyService {
         popupP.classList.add('consent');
         popupBtn.classList.add('cookieService-btn');
 
-        popupP.innerHTML = `Plikami cookies stosowanymi na naszej stronie można zarządzać, w tym je usunąć za pośrednictwem ustawień przeglądarki internetowej. Internauci mogą dowolnie zarządzać stosowanymi plikami <a href="${url}">(polityka prywatności)</a>`;
+        popupP.innerHTML = `Plikami cookies stosowanymi na naszej stronie można zarządzać, w tym je usunąć za pośrednictwem ustawień przeglądarki internetowej. Internauci mogą dowolnie zarządzać stosowanymi plikami <a href="${url}" style="color: rgb(12, 242, 93)!important;">(polityka prywatności)</a>`;
         popupBtn.innerHTML = `Ok`;
 
         popupWrapper.appendChild(popupContainer);
@@ -170,4 +170,4 @@ class CookiePolicyService {
 
 }
 
-service = new CookiePolicyService;
+service = new CookiePolicyService('https://www.pcwoenergy.pl/polityka-prywatnosci-2/');
